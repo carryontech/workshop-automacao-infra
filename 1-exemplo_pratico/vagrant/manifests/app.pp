d1472b94e03f1daa22349b1d9bee3b01ca814404
Luciano Antônio <lucianotkd00@gmail.com>
2019-10-02 17:21:29 -0300

    Inserindo comentários

--- 1-exemplo_pratico/vagrant/manifests/app.pp
+++ 1-exemplo_pratico/vagrant/manifests/app.pp
@@ -1,12 +1,16 @@
 # Autor: CarryOn Tech
+
+# Atualizar as listas de pacotes disponíveis
 exec { "apt-update":
   command => "/usr/bin/apt-get update"
 }
 
+# Instalar o Apache
 package { ["apache2"]:
   ensure => installed
 }
 
+# Criar o service do Apache
 service { "apache2":
   ensure => running,
   enable => true,
@@ -15,7 +19,7 @@ service { "apache2":
   require => Package["apache2"]
 
 }
-
+# Criar a pasta "/var/www/html/site" e copiar os arquivos do site para dentro desta pasta
 exec { ["copyfiles"]:
   command => "mkdir /var/www/html/site && cp -r /vagrant/manifests/site/* /var/www/html/site",
   path => "/bin/",
@@ -24,6 +28,7 @@ exec { ["copyfiles"]:
   notify => Service["apache2"]
 }
 
+# Instalar o PHP e os drivers necessários para a conexão com o MySQL
 package { ["php5","php5-mysql","php5-sybase"]:
   ensure => installed,
   notify => Service["apache2"]
--- 1-exemplo_pratico/vagrant/manifests/db.pp
+++ 1-exemplo_pratico/vagrant/manifests/db.pp
@@ -1,12 +1,14 @@
 # Autor: CarryOn Tech
+
+# Atualizar as listas de pacotes disponíveis
 exec { "apt-update":
   command => "/usr/bin/apt-get update"
 }
-
+# Instalar o MySQL Server
 package { ["mysql-server"]:
   ensure => installed
 }
-
+# Criar o service do MySQL Server
 service { "mysql":
     ensure => running,
     enable => true,
@@ -15,7 +17,9 @@ service { "mysql":
     require => Package["mysql-server"]
 }
 
-# Liberando acesso remoto para que a instância app possa se conectar ao banco
+# Liberar acesso remoto para que a instância app possa se conectar ao banco
+# Obs: Essa configuração também pode ser feita configurando regras de firewall
+# Porém, para esse Workshop, a configuração abaixo basta
 
 exec { "liberaracessoremoto":
     command => "sed -i 's/bind-address/#bind-address/' /etc/mysql/my.cnf",
@@ -26,7 +30,7 @@ exec { "liberaracessoremoto":
 
 
 
-# Criando o banco de dados com o nome agenda
+# Criar o banco de dados com o nome "agenda"
 exec {"criarbanco":
   command => "mysqladmin -u root create agenda",
   unless => "mysql -u root agenda",
@@ -34,6 +38,7 @@ exec {"criarbanco":
   require => Package["mysql-server"]
 }
 
+#  Criar o arquivo "/opt/scripbanco.sql" a partir do script sql disponível no diretório "manifests" do host hospedeiro
 file { "/opt/scriptbanco.sql":
     source => "/vagrant/manifests/site/pessoa.sql",
     owner => "mysql",
@@ -42,6 +47,7 @@ file { "/opt/scriptbanco.sql":
     require => Package["mysql-server"]
 }
 
+# Criar o usuário "agenda" e dar acesso total para esse usuário ao banco "agenda"
 exec { "mysql-password" :
     command => "mysql -uroot -e \"GRANT ALL PRIVILEGES ON * TO 'agenda'@'%' IDENTIFIED BY 'agenda';\" agenda",
     unless  => "mysql -uagenda -pminha-senha agenda",
@@ -49,6 +55,7 @@ exec { "mysql-password" :
     require => Exec["criarbanco"]
 }
 
+# Executar o script "pessoa.sql" para criar a tabela "pessoa" no banco de dados "agenda"
 exec {"criarestrutura":
   command => "mysql -uagenda -pagenda < /vagrant/manifests/site/pessoa.sql",
   path => "/usr/bin",
